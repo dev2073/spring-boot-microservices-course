306d8cb56c6aab1e069bef7de1e91b61948b04cf
dev2073 <dev2073@protonmail.com>
2025-06-19 23:02:43 +0200

    Add comment to handleUnhandledException method in GlobalExceptionHandler

--- catalog-service/src/main/java/com/keelient/catalog_service/exceptions/GlobalExceptionHandler.java
+++ catalog-service/src/main/java/com/keelient/catalog_service/exceptions/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String SERVICE_NAME = "catalog-service";
 
     @ExceptionHandler(Exception.class)
-    ProblemDetail handleUnhandledException(Exception e) {
+    ProblemDetail handleUnhandledException(Exception e) {//dcfds
         ProblemDetail problemDetail =
                 ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         problemDetail.setTitle("Internal Server Error");
