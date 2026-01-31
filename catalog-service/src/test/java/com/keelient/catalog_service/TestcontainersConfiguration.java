be986a6ffc39b798b01e1e6f93175da730920805
dev2073 <dev2073@protonmail.com>
2026-01-31 20:08:27 +0100

    Add initial project setup with Maven and Docker Compose configuration
    - Created a Docker Compose file for Tempo with server, distributor, ingester, compactor, and storage configurations.
    - Added Maven Wrapper scripts (mvnw and mvnw.cmd) for easier project setup and execution.
    - Introduced a parent POM file with project metadata and Spotless plugin for code formatting.
    - Configured Spotless plugin to enforce Java formatting rules during the build process.

---
+++ catalog-service/src/test/java/com/keelient/catalog_service/TestcontainersConfiguration.java
@@ -0,0 +1,24 @@
+package com.keelient.catalog_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.grafana.LgtmStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    LgtmStackContainer grafanaLgtmContainer() {
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+}
